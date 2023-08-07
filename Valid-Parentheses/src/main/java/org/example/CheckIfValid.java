7b72199913d67f902b830948356bba6c1a8f9385
Philip Vollmann <Philip.vollmann0408@gmail.com>
2023-08-07 12:36:27 +0200

    kata "Valid Parentheses"

---
+++ Valid-Parentheses/src/main/java/org/example/CheckIfValid.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class CheckIfValid {
+
+    public static boolean isValidParentheses(String s) {
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                count++;
+            } else if (c == ')') {
+                count--;
+                if (count < 0) {
+                    return false;
+                }
+            }
+        }
+        return count == 0;
+    }
+
+}
